492864eb3073c3c1a9790c833316bbac8600f76f
Edyta Chrzanowska <edytapatrycja.chrzanowska@gmail.com>
2022-04-14 16:37:41 +0200

    project init. closes #1 closes #20

---
+++ src/test/java/com/system/management/library/librarymanagementsystem/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.management.library.librarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
