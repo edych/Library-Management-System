a319dfbe0e5b6a47c37a484d275eded922408988
Edyta Chrzanowska <edytapatrycja.chrzanowska@gmail.com>
2022-04-16 21:12:39 +0200

    add Vaadin. closes #24 closes #23 closes #17

--- src/main/java/com/system/management/library/librarymanagementsystem/LibraryManagementSystemApplication.java
+++ src/main/java/com/system/management/library/librarymanagementsystem/LibraryManagementSystemApplication.java
@@ -1,10 +1,14 @@
 package com.system.management.library.librarymanagementsystem;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LibraryManagementSystemApplication {
+@Theme(value = "librarymanagementsystem")
+public class LibraryManagementSystemApplication extends SpringBootServletInitializer implements AppShellConfigurator {
 
 	public static void main(String[] args) {
 
