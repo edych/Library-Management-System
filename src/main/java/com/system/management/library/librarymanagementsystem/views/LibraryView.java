e8151e8365e1c92fba1e7ff8f934ae7b4224468e
Edyta Chrzanowska <edytapatrycja.chrzanowska@gmail.com>
2022-04-16 23:35:35 +0200

    When no records are stored then information ‘No records’ will be displayed.

--- src/main/java/com/system/management/library/librarymanagementsystem/views/LibraryView.java
+++ src/main/java/com/system/management/library/librarymanagementsystem/views/LibraryView.java
@@ -3,6 +3,7 @@ package com.system.management.library.librarymanagementsystem.views;
 import com.system.management.library.librarymanagementsystem.model.Book;
 import com.system.management.library.librarymanagementsystem.repository.BookRepository;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -18,10 +19,25 @@ public class LibraryView extends HorizontalLayout {
 
         this.grid = new Grid<>(Book.class);
         this.bookRepository = bookRepository;
-        add(grid);
-        listBooks();
 
         setMargin(true);
+
+        if (bookRepository.findAll().isEmpty()) {
+
+            Div div = new Div();
+            div
+                    .getElement()
+                    .setProperty("innerHTML",
+                            "<h2>No records</h2><br><p>Use <i><b>Add new record</i></b> to fill this space.</p>");
+
+            add(div);
+            setSizeFull();
+            setJustifyContentMode(JustifyContentMode.CENTER);
+            getStyle().set("text-align", "center");
+        } else {
+            add(grid);
+            listBooks();
+        }
     }
 
     private void listBooks() {
